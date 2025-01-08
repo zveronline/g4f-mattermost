df2305e66ac18e667c9bee1c424b22bd0a76fa53
Артем <mail@zveronline.ru>
2025-01-08 16:42:48 +0000

    tabs

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ from plugin_gpt_img import GPTimg
 from plugin_help import Help
 
 bot = Bot(
-    settings=Settings(
-        MATTERMOST_URL = "https://mattermost.zveronline.ru",
-#        MATTERMOST_PORT = 443,
-        BOT_TOKEN = "9huy5zqt1in9jj79db7hiydpyw",
-        BOT_TEAM = "z-network",
- #       SSL_VERIFY = True,
-    ),  # Either specify your settings here or as environment variables.
-    plugins=[GPTtext(), GPTimg(), Help()],  # Add your own plugins here.
+	settings=Settings(
+		MATTERMOST_URL = "https://mattermost.zveronline.ru",
+        MATTERMOST_PORT = 443,
+		BOT_TOKEN = "9huy5zqt1in9jj79db7hiydpyw",
+		BOT_TEAM = "z-network",
+        SSL_VERIFY = True,
+	),  # Either specify your settings here or as environment variables.
+	plugins=[GPTtext(), GPTimg(), Help()],  # Add your own plugins here.
 )
 bot.run()
