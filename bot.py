c98eb76bdfd484e86dcd33cb0802ba40638fe6b0
Артем <mail@zveronline.ru>
2025-01-08 14:30:53 +0000

    upd

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = Bot(
     settings=Settings(
         MATTERMOST_URL = "https://mattermost.zveronline.ru",
 #        MATTERMOST_PORT = 443,
-        BOT_TOKEN = "wtoken",
+        BOT_TOKEN = "9huy5zqt1in9jj79db7hiydpyw",
         BOT_TEAM = "z-network",
  #       SSL_VERIFY = True,
     ),  # Either specify your settings here or as environment variables.
